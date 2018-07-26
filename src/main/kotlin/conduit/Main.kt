8a3b7e2f91f5bcdfd83393f917906450cf5237c2
Ali Sabzevari <alisabzevari@gmail.com>
2018-07-26 23:24:06 +0200

    Add logging

--- src/main/kotlin/conduit/Main.kt
+++ src/main/kotlin/conduit/Main.kt
@@ -4,6 +4,8 @@ import conduit.handlers.LoginHandler
 import conduit.model.LoginRequest
 import conduit.model.LoginResponse
 import conduit.repository.ConduitRepository
+import mu.KotlinLogging
+import org.apache.logging.log4j.core.config.Configurator
 import org.http4k.core.Body
 import org.http4k.core.Method
 import org.http4k.core.Response
@@ -21,10 +23,14 @@ import org.jetbrains.exposed.sql.Database
 // TODO: Fix content type of request
 // TODO: Configure json serializer correctly
 // TODO: Define a better place for request lenses
-// TODO: Add log4j
+
 
 
 fun main(args: Array<String>) {
+
+    Configurator.initialize(null, "log4j2-local.yaml")
+
+    val logger = KotlinLogging.logger("main")
 
     val database = Database.connect("jdbc:h2:~/conduit", driver = "org.h2.Driver")
 
@@ -45,5 +51,8 @@ fun main(args: Array<String>) {
         }
     )
 
-    app.asServer(Jetty(9000)).start().block()
+
+    logger.info("Starting server...")
+    app.asServer(Jetty(9000)).start()
+    logger.info("Server started on port 9000")
 }
