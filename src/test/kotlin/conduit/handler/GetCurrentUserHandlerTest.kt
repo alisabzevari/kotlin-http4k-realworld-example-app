b9b8c19940a9361108a3972c9140f8c794b7410a
Ali Sabzevari <alisabzevari@gmail.com>
2018-09-30 18:44:28 +0200

    Add tests for GetCurrentUserHandler

---
+++ src/test/kotlin/conduit/handler/GetCurrentUserHandlerTest.kt
@@ -0,0 +1,61 @@
+package conduit.handler
+
+import conduit.model.*
+import conduit.util.TokenAuth
+import conduit.util.hash
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.impl.DefaultClaims
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class GetCurrentUserHandlerTest {
+    lateinit var unit: GetCurrentUserHandlerImpl
+
+    @BeforeEach
+    fun beforeEach() {
+        unit = GetCurrentUserHandlerImpl(
+            repository = mockk(relaxed = true)
+        )
+    }
+
+    @Test
+    fun `should return user info based on tokenInfo`() {
+        val password = Password("password")
+        val dbUser = User(
+            0,
+            Email("jake@jake.jake"),
+            password.hash(),
+            null,
+            Username("jake"),
+            Bio("I work at statefarm"),
+            Image("an image url")
+        )
+        every { unit.repository.findUserByEmail(any()) } returns dbUser
+
+        val tokenInfo = TokenAuth.TokenInfo(Token("token"), DefaultClaims(mapOf("email" to "email@site.com")))
+
+        val result = unit(tokenInfo)
+
+        assertEquals(tokenInfo.token, result.token)
+        assertEquals(dbUser.bio, result.bio)
+        assertEquals(dbUser.email, result.email)
+        assertEquals(dbUser.image, result.image)
+        assertEquals(dbUser.username, result.username)
+    }
+
+    @Test
+    fun `should throw exception if user not found`() {
+        every { unit.repository.findUserByEmail(any()) } returns null
+
+        val tokenInfo = TokenAuth.TokenInfo(Token("token"), DefaultClaims(mapOf("email" to "email@site.com")))
+
+        assertThrows<UserNotFoundException> {
+            unit(tokenInfo)
+        }
+    }
+
+}
