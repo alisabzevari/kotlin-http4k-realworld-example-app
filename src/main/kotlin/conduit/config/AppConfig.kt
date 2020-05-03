f3c45513a8cc546a85df7fc76835a1510c9c4a8e
Jeff Johnson <jeffcjohnson@gmail.com>
2020-05-03 12:40:44 -0400

    Add CrossOrigin Resource Sharing (CORS)
    This is required for the client to connect from a browser.

--- src/main/kotlin/conduit/config/AppConfig.kt
+++ src/main/kotlin/conduit/config/AppConfig.kt
@@ -1,8 +1,11 @@
 package conduit.config
 
+import org.http4k.filter.CorsPolicy
+
 data class AppConfig(
     val logConfig: String,
     val db: DbConfig,
+    val corsPolicy: CorsPolicy,
     val port: Int
 )
 
--- src/main/kotlin/conduit/config/local.kt
+++ src/main/kotlin/conduit/config/local.kt
@@ -1,10 +1,19 @@
 package conduit.config
 
+import org.http4k.core.Method
+import org.http4k.filter.CorsPolicy
+
 val local = AppConfig(
     logConfig = "log4j2-local.yaml",
     db = DbConfig(
         url = "jdbc:h2:~/conduit-db/conduit",
         driver = "org.h2.Driver"
     ),
-    port = 9000
+    port = 9000,
+    corsPolicy = CorsPolicy(
+        origins = listOf("localhost:9000"),
+        headers = listOf("content-type", "authorization"),
+        methods = Method.values().toList(),
+        credentials = true
+    )
 )
